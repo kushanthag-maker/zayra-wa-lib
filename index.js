733ae4c4e98abc537cad7f5e57d6c4b4c79c5cd7
kushanthag-maker <kushanthag@gmail.com>
2026-06-21 07:44:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
+export * from "@whiskeysockets/baileys";
+export { default } from "@whiskeysockets/baileys";
+
+// Zayra custom exports
 export { startConnection } from "./connection.js";
 export { sendButtons } from "./buttons.js";
